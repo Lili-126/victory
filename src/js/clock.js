f5b661e3d98ce0a0df924b2078e0c57a2c7445f4
Liliia <gabdulahatov_na@mail.ru>
2025-05-13 14:53:31 +0400

    feat: added features to project victory

---
+++ src/js/clock.js
@@ -0,0 +1,20 @@
+const hourArrow = document.querySelector('.hours');
+const minuteArrow = document.querySelector('.minutes');
+const secondArrow = document.querySelector('.seconds');
+let deg = 6; //шаг минутной и секундной стрелок
+
+/* export */ const getClock = () => {
+  const date = new Date();
+  let hours = date.getHours() * 30;  // часы умнажаем на 30 градусов (шаг часа = 30 градусов)
+  let minutes = date.getMinutes() * deg;
+  let seconds = date.getSeconds() * deg;
+
+  hourArrow.style.transform = `rotateZ(${hours + (minutes  / 12)}deg)`;
+  minuteArrow.style.transform = `rotateZ(${minutes}deg)`;
+  secondArrow.style.transform = `rotateZ(${seconds}deg)`;
+}
+
+ setInterval( () => {
+  getClock();
+}, 0);
+
